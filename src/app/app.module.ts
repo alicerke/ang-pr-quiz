26ef5aeffd029d1c97010643a2a772942748b953
alicerke <istvan.felhelyes@gmail.comm>
2021-03-30 12:07:52 +0200

    creat component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './common/navigation/navigation.component';
+import { AdminComponent } from './page/admin/admin.component';
+import { HomeComponent } from './page/home/home.component';
+import { QuizComponent } from './page/quiz/quiz.component';
+import { QuizEditorComponent } from './page/quiz-editor/quiz-editor.component';
+import { QuestionEditorComponent } from './page/question-editor/question-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    AdminComponent,
+    HomeComponent,
+    QuizComponent,
+    QuizEditorComponent,
+    QuestionEditorComponent
   ],
   imports: [
     BrowserModule,
